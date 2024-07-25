3a482ab20562229c33b7c19e8eb094659238fece
kimssirr <kimssirr@naver.com>
2024-07-26 01:22:57 +0900

    3주차 공부

--- src/파트8/TaxiExam.java
+++ src/파트8/TaxiExam.java
@@ -4,7 +4,7 @@ public class TaxiExam{
     public static void main(String []args){
         Taxi taxi = new Taxi();
         taxi.BASE_FARE = 2500;
-        Meter taxi2 = new Taxi();
+        Meter taxi2 = new Taxi(); //Meter에 BASE_FARE가 있을시
         ((Taxi) taxi2).BASE_FARE = 2500; // 캐스팅 후 접근
     }
 }
